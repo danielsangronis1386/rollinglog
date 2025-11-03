a677e37779e20088db9603725299d3bc118fd9a2
danielsangronis1386 <daniel.sangronis1386@gmail.com>
2025-11-02 23:21:16 -0800

    review model and Add new model: Review

--- rollinglog/catalog/admin.py
+++ rollinglog/catalog/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Brand, RollingPaper
+from .models import Brand, RollingPaper, Review
+
 
 # Register your models here.
 
 admin.site.register(Brand)
 admin.site.register(RollingPaper)
+admin.site.register(Review)
