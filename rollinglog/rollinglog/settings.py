4e503a5a33601c1dc8961d41fe9dc45852ab766c
danielsangronis1386 <daniel.sangronis1386@gmail.com>
2025-11-03 23:21:35 -0800

    allow media uploads

--- rollinglog/rollinglog/settings.py
+++ rollinglog/rollinglog/settings.py
@@ -125,3 +125,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'paper-index'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- rollinglog/rollinglog/urls.py
+++ rollinglog/rollinglog/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from catalog import views as catalog_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')), #directs traffic to catalog app
     path('account/signup/', catalog_views.signup, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
